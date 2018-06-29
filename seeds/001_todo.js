f9a89d6c1d85d46b7744d24557a2728ef1384a55
Priscilla Okawa <prisoka@gmail.com>
2018-06-28 20:04:42 -0700

    added seed, added node engine to package.json, wip

---
+++ seeds/001_todo.js
@@ -0,0 +1,24 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('todo').del()
+    .then(function () {
+      const todos = [{
+        title: 'Build a CRUD App',
+        priority: 1,
+        date: new Date()
+      }, {
+        title: 'Learn Heroku',
+        priority: 1,
+        date: new Date()
+      }, {
+        title: 'Learn Authentication and Authorization',
+        priority: 1,
+        date: new Date()
+      }, {
+        title: 'Rest',
+        priority: 5,
+        date: new Date()
+      }];
+    });
+};
